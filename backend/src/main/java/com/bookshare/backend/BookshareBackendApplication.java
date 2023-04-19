0c63d77054033c7f2e20ec712e46ea996b1b306b
Aaron Lambert <aeblambert@gmail.com>
2023-04-19 20:47:19 +0200

    Create backend Spring Boot project with the following dependencies: Spring Web, Spring Security, Spring Data JPA

---
+++ backend/src/main/java/com/bookshare/backend/BookshareBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bookshare.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookshareBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookshareBackendApplication.class, args);
+    }
+
+}
